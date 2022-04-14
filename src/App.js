da8043e9aa080823d1cc4bf1d9feb387e99a67b8
Andrés Pujol <andrespujol@gmail.com>
2022-04-14 00:02:10 -0300

    crear landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { ItemListContainer } from './Components/ItemListContainer/ItemListContainer';
 import { NavBar } from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer greeting='Tienda' />
     </div>
   );
 }
---
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <div>
+        <h1 className='text-4xl'>{ greeting }</h1>
+    </div>
+  )
+}
